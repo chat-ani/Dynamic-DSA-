33061f219df51f4998e343d76c6279a7c8799f3d
Anirban Chatterjee <109433055+chat-ani@users.noreply.github.com>
2022-07-16 23:47:46 +0530

    lcs commited

---
+++ lcs.cpp
@@ -0,0 +1,10 @@
+Hi everyone today here I'm going to discuss the very famous dynamic problem Longest Common Subsequence.
+
+Problem Statement : 
+       We have to find the LCS of given two strings.
+       Inputs: 
+       1] string a = "abcdeswd"
+          string b = "bcfesewqd"
+       Outputs:
+       2]
+     
